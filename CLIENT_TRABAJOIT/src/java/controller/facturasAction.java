8af6a5d0f1c2a0acf11c8a2efa611c5b8f4007e8
AOcchietti <aarzocc@alu.upo.es>
2025-06-04 20:51:18 +0200

    Reestructuración

---
+++ CLIENT_TRABAJOIT/src/java/controller/facturasAction.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import com.opensymphony.xwork2.ActionSupport;
+import java.util.*;
+import model.*;
+
+/**
+ *
+ * @author Ariel
+ */
+public class facturasAction extends ActionSupport {
+    
+    private List <Factura> facturas;
+    
+    public facturasAction() {
+    }
+    
+    public String execute() throws Exception {
+        
+    }
+    
+}
